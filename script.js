3298099be90306ffa759266d2fe51ffd5355c2ff
duy <duy123galaxy13@gmail.com>
2019-05-14 12:34:44 +0700

    xog login
    dang bi bug collapse click ko hoat dong

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 console.log("conneted");
 
+// TODO: Phan signIn va gets starteds
 document.getElementById('sign_in').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+	document.querySelector('#test1').style.display = "flex";
 });
 
 document.getElementById('start').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+	document.querySelector('#test1').style.display = "flex";
 });
 
 document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
+	document.querySelector('#test1').style.display = "none";
+});
+
+// TODO: phan login
+document.getElementById('log_in').addEventListener("click", function(){
+	document.querySelector('#test2').style.display = "flex";
+});
+document.querySelector('#login_close').addEventListener("click", function() {
+	document.querySelector('#test2').style.display = "none";
 });
